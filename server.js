9d3f14fb58ea444635b283c83699bd6606fd457d
Peter <naughtnowwhen@gmail.com>
2018-12-29 11:48:16 -0800

    second commit, Lorin added some basic ejs styling and now we are trying to get /hello to serve our ejs path

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 'use strict';
 const express = require('express');
+const superagent = require('superagent');
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs')
+
+
+// load env vars =====================
+require('dotenv').config;
+const PORT = process.env.PORT;
+//====================================
 
 app.get('/', function (req, res){
   res.send('hello to you world, hope you are having a nice day.');
 });
 
-app.listen(3000);
+app.get('/hello', function (req,res){
+  res.send('is it working');
+  res.render('views/pages/index.ejs');
+});
+
+app.listen(PORT, () => {
+    console.log(`running on port: ${PORT}`);
+});
 
--- trialServer.js
+++ trialServer.js
@@ -1,9 +1,53 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', function (req, res){
+//   res.send('hello to you world, hope you are having a nice day.');
+// });
+
+// app.listen(3000);
+
 const express = require('express');
+const supperagent = require('superagent');
+
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', function (req, res){
-  res.send('hello to you world, hope you are having a nice day.');
-});
+app.set('view engine', 'ejs')
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
 
+
+app.get('/', home);
+
+app.post('/searches', search)
+
+function home(req, res){
+  res.render('pages/index');
+}
+
+function search(req, res){
+  const searchStr = req.body.search[0];
+  const searchType = req.body.search[1];
+  let URL = 'https://www.goolgeapis.com/books/v1/volumes?q='
+
+  if(searchType === 'title'){
+    URL += `+intitle:${searchStr}`;
+  } else if(searchType === 'author'){
+    URL += `+inauthor:${searchStr}`
+  }
+
+  return supperagent.get(URL)
+    .then(result => {
+      let books = result.body.items.map(book => new Book);
+      console.log(books)
+      res.render('pages/show', {books})
+    })
+
+}
+
+function Book(book){
+  this.title = book.volume.title || 'Thisw book does not have a title';
+}
+
+app.listen(PORT, () => console.log(`app is running on PORT : ${PORT}`))
